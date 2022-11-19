efc2c15ca928664eed71472cca47a23f78399183
rakhimov23 <111849985+rakhimov23@users.noreply.github.com>
2022-11-19 11:43:53 +0500

    javacript

---
+++ app.js
@@ -0,0 +1,19 @@
+let ism = prompt("Ismizngiz:");
+let familya = prompt("Familyangiz:");
+let yosh = prompt("Yoshingiz:");
+let jins = prompt("Jinsingiz:");
+let nomer = prompt("Tel raqamingiz:");
+let card = prompt("Cartangiz:");
+let email = prompt("Email:");
+let shahar = prompt("Yashah joyingiz:");
+
+
+console.log(`Ismingiz:, ${ism} \n
+    Familyangiz: ${familya} \n
+    Yoshingiz: ${yosh} \n
+    Jinsingiz: ${jins} \n
+    Tel raqamingiz: ${nomer} \n
+    Cartangiz: ${card} \n
+    Email: ${email} \n
+    Yashah joyingiz: ${shahar} \n
+    `);
